01a56aeb86e44e652812e3eed50d5d61066acb29
Marko Milovic <mmilovic21@gmail.com>
2022-09-03 17:06:18 +0100

    second automation, expect url to contain search

--- test/specs/udemy.e2e.js
+++ test/specs/udemy.e2e.js
@@ -10,4 +10,14 @@ describe("Assessment", function () {
     // assertion for 1
     await expect(browser).toHaveTitleContaining("Google");
   });
+
+  it("searches for test automation learning in google and displays correct results", async function () {
+    // 2
+    const searchMessage = "Test Automation Learning";
+    await UdemyPage.gSearch.setValue(searchMessage);
+    await UdemyPage.gSearch.keys("Enter");
+
+    // assertion 2
+    await expect(browser).toHaveUrlContaining("Test+Automation+Learning");
+  });
 });
